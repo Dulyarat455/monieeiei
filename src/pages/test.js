9d0d40e695a3cc80fea23953162d7ba82675821f
Dulyarat455 <Dulyarat.455@gmail.com>
2023-10-11 22:37:09 +0700

    page

---
+++ src/pages/test.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+
+
+const Test = () => {
+     const [check, setCheck] = useState([{
+        "User_id": 1,
+        "User_firstname": "",
+        "user_lastname": ""
+    }])
+    
+     const [ans, setAns] = useState(null)
+  
+    
+      useEffect(() => {
+        
+       
+        
+            const res =  fetch(`/api/test`,
+               {
+                 method: "GET",
+                
+               }
+             ).then((res) => res.json())
+             .then((data) => {
+               console.log(data.ans)
+               setCheck(data.ans)
+             }
+             );
+           
+       
+      }, []);
+
+
+      console.log("check = ",check[0]["user_id"])
+
+
+
+    return (
+        <div>
+          {/* <p>hello world</p> */}
+          {/* {check && (check)} */}
+          { check &&  (check[0]["user_lastname"])}
+        </div>
+      );
+
+}
+
+export default Test;
