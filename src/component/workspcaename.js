489ef2d716d6c3ae8703dd4f963342a2361627e9
Meuracha <uracharittikulsittichai@gmail.com>
2023-10-30 22:28:29 +0700

    .

---
+++ src/component/workspcaename.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+import { Rubik } from 'next/font/google'
+const inter = Rubik({ subsets: ['latin'],weight:['400'] })
+
+export default function workspace_name()
+{
+    return(
+        <></>
+    )
+}
