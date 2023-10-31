3ee904cade1ee18411a7594648616a38137188c0
Meuracha <uracharittikulsittichai@gmail.com>
2023-11-01 01:47:22 +0700

    edit navbarbottom icon and size

--- src/component/navbarbottom.js
+++ src/component/navbarbottom.js
@@ -5,25 +5,25 @@ import { useRouter } from 'next/router';
 
 export default function Navbarbottom(){
 
-    const [activeItemPrivate, setActiveItemPrivate] = useState('black');
-    const [activeItemWorkspace, setActiveItemWorkspace] = useState('black');
     const [activeItemAccount, setActiveItemAccount] = useState('black');
+    const [activeItemWorkspace, setActiveItemWorkspace] = useState('black');
+    const [activeItemSetting, setActiveItemSetting] = useState('black');
     const router = useRouter();
     const currentPath = router.asPath;
     console.log(currentPath);
 
     useEffect(() => {
-        if(currentPath === "/private"){
-            setActiveItemPrivate('#9747FF')
+        if(currentPath === "/account"){
+            setActiveItemAccount('#9747FF')
             setActiveItemWorkspace('black')
-            setActiveItemAccount('black')
+            setActiveItemSetting('black')
         }else if(currentPath === "/workspace"){
             setActiveItemWorkspace('#9747FF')
-            setActiveItemPrivate('black')
             setActiveItemAccount('black')
-        }else if(currentPath === "/account"){
-            setActiveItemAccount('#9747FF')
-            setActiveItemPrivate('black')
+            setActiveItemSetting('black')
+        }else if(currentPath === "/setting"){
+            setActiveItemSetting('#9747FF')
+            setActiveItemAccount('black')
             setActiveItemWorkspace('black')
         }
     }, [currentPath]);
@@ -44,8 +44,8 @@ export default function Navbarbottom(){
       };
     
       const iconStyles = {
-        width: '30px',
-        height: '30px',
+        width: '40px',
+        height: '40px',
         flexShrink: 0,
         display: 'flex',
         margin: '0 10px',
@@ -55,48 +55,7 @@ export default function Navbarbottom(){
     
     return(
         <nav style={navbarStyles_bottom} className="inline-felx">
-            <Link href={"/private"}>
-                <div 
-                style={iconStyles}
-                onClick={() => setActiveItemPrivate("private")}
-                data-item="private"
-                title="private">
-                <svg 
-                xmlns="http://www.w3.org/2000/svg" 
-                fill="none" 
-                viewBox="0 0 26 26" 
-                stroke-width="1.5" 
-                stroke={activeItemPrivate}
-                class="w-6 h-6">
-                    <path 
-                    stroke-linecap="round" 
-                    stroke-linejoin="round" 
-                    d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                </svg>
-                </div>
-            </Link>
-            <Link href={"/workspace"}>
-                <div 
-                style={iconStyles}
-                onClick={() => setActiveItemWorkspace("workspace")}
-                data-item="workspace"
-                title="workspace">
-                <svg 
-                xmlns="http://www.w3.org/2000/svg" 
-                fill="none" 
-                viewBox="0 0 26 26" 
-                stroke-width="1.5" 
-                stroke={activeItemWorkspace}
-                class="w-6 h-6">
-                    <path 
-                    stroke-linecap="round" 
-                    stroke-linejoin="round" 
-                    d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
-                </svg>
-
-                </div>
-            </Link>
-            <Link href={"/account"} className="mr-14">     
+            <Link href={"/account"}>
                 <div 
                 style={iconStyles}
                 onClick={() => setActiveItemAccount("account")}
@@ -108,7 +67,49 @@ export default function Navbarbottom(){
                 viewBox="0 0 26 26" 
                 stroke-width="1.5" 
                 stroke={activeItemAccount}
-                class="w-6 h-6">
+                class="w-8 h-8">
+                    <path 
+                    stroke-linecap="round" 
+                    stroke-linejoin="round" 
+                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                </svg>
+
+                </div>
+            </Link>
+            <Link href={"/workspace"}>
+                <div
+                style={iconStyles}
+                onClick={() => setActiveItemWorkspace("workspace")}
+                data-item="workspace"
+                title="workspace">
+                <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 26 26" 
+                stroke-width="1.5" 
+                stroke={activeItemWorkspace}
+                class="w-8 h-8">
+                    <path 
+                    stroke-linecap="round" 
+                    stroke-linejoin="round" 
+                    d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z" />
+                </svg>
+
+                </div>
+            </Link>
+            <Link href={"/setting"} className="mr-14">     
+                <div 
+                style={iconStyles}
+                onClick={() => setActiveItemSetting("setting")}
+                data-item="setting"
+                title="setting">
+                <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 26 26" 
+                stroke-width="1.5" 
+                stroke={activeItemSetting}
+                class="w-8 h-8">
                     <path 
                     stroke-linecap="round" 
                     stroke-linejoin="round" 
