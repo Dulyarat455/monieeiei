31411afecda745523d1977192246158723ab4930
Meuracha <uracharittikulsittichai@gmail.com>
2023-11-03 01:21:29 +0700

    Add slide bar component and new page

---
+++ src/components/slidebar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function Slidebar(){
+    return(
+        <div className="ml-10 mt-4"> 
+            slidebar
+        </div>
+    )
+}
---
+++ src/pages/workspace_dailyexpense_form.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Workspace_dailyexpense_form(){
+    return(
+        <div>Daily expense form</div>
+    )
+}
