2e7f417d881f05115192f71c0317220cd99ae795
Meuracha <uracharittikulsittichai@gmail.com>
2023-11-06 02:40:44 +0700

    edit workspacename

--- src/components/workspacename.js
+++ src/components/workspacename.js
@@ -12,13 +12,13 @@ export default function Workspacename({workspace_name, workspace_id , owner_stat
 
     return(
         <div className="workspace-container mt-4 relative">
-            <button style={{ width: '300px', height: '80px'}} className="bg-[#FAE392] hover:bg-[#FFDF6F] rounded-md justify-center items-center gap-2.5 inline-flex">
+            <button style={{ width: '300px', height: '80px'}} className="bg-[#FAE392] hover:bg-[#FFDF6F] rounded-2xl justify-center items-center gap-2.5 inline-flex">
                 <div style={{fontSize: 26}} className={`text-lg text-[#9B7C0D] hover:text-[#9B7C0D] font-normal font-rubik justify-center items-center ${inter.className}`}>
                     {workspace_name ? workspace_name : "Workspace’s Name"}
                 </div>
             </button>
             {owner_status === 1 &&
-            (<span className="w-10 absolute top-0 right-2 h-[22px] absolute mt-2 top-30  text-xs text-[#219226] font-normal font-rubik px-2 py-1 bg-[#C5FFC7] bg-opacity-75 rounded justify-center items-center" >
+            (<span className="w-10 absolute top-1 right-3 h-[22px] absolute mt-2 top-30  text-xs text-[#219226] font-normal font-rubik px-2 py-1 bg-[#C5FFC7] bg-opacity-75 rounded justify-center items-center" >
                   own
                 </span>)
             }
