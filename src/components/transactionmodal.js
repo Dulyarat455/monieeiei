052284aad2b5e1b3600ba270c035ca28213eb9fa
Meuracha <uracharittikulsittichai@gmail.com>
2023-11-06 00:00:07 +0700

    Update transactionmodal

--- src/components/transactionmodal.js
+++ src/components/transactionmodal.js
@@ -5,11 +5,11 @@ export default function Transactionmodal({isVisible, onClose}) {
 
     return(
         <div className="fixed inset-0 bg-[#757575] bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
-            <div style={{width: '315px', height: '400px',borderRadius:'1.5rem'}} className="h-full bg-[#FAE392] border border-[#9B7B0C] flex flex-col justify-center items-center">
+            <div style={{position:'relative', width:'315px', height:'auto', paddingBottom:'18px',paddingTop:'18px'}} className="h-full bg-[#FAE392] rounded-3xl border border-[#9B7B0C] flex flex-col justify-center items-center">
                 {/* Query Transaction's name */}
-                <div style={{fontSize: '22px'}} className="text-center text-stone-900 text-2xl font-medium font-['Rubik']">Transaction’s name</div>
+                <div className="text-center text-stone-900 text-2xl font-medium font-['Rubik']">Transaction’s name</div>
                 {/* Query Date/Month/Year Time */}
-                <div style={{fontSize: '20px'}} className="text-yellow-700 text-xl font-normal font-['Rubik']">10 Oct 2023 8.35 PM</div>
+                <div className="text-yellow-700 text-xl font-normal font-['Rubik']">10 Oct 2023 8.35 PM</div>
                 <div className="flex-col justify-start items-start gap-3 flex">
                     <div className="justify-start items-center gap-2 inline-flex mt-4">
                         <div className="flex-col justify-start items-start gap-1 inline-flex">
@@ -50,6 +50,21 @@ export default function Transactionmodal({isVisible, onClose}) {
                             <div className="px-2 py-1 bg-purple-300 bg-opacity-75 rounded justify-center items-center gap-2.5 inline-flex">
                                 <div className="text-violet-800 text-xs font-normal font-['Rubik']">Username1</div>
                             </div>
+                            <div className="px-2 py-1 bg-purple-300 bg-opacity-75 rounded justify-center items-center gap-2.5 inline-flex">
+                                <div className="text-violet-800 text-xs font-normal font-['Rubik']">Username1</div>
+                            </div>
+                            <div className="px-2 py-1 bg-purple-300 bg-opacity-75 rounded justify-center items-center gap-2.5 inline-flex">
+                                <div className="text-violet-800 text-xs font-normal font-['Rubik']">Username1</div>
+                            </div>
+                            <div className="px-2 py-1 bg-purple-300 bg-opacity-75 rounded justify-center items-center gap-2.5 inline-flex">
+                                <div className="text-violet-800 text-xs font-normal font-['Rubik']">Username1</div>
+                            </div>
+                            <div className="px-2 py-1 bg-purple-300 bg-opacity-75 rounded justify-center items-center gap-2.5 inline-flex">
+                                <div className="text-violet-800 text-xs font-normal font-['Rubik']">Username1</div>
+                            </div>
+                            
+                            
+                            
                         </div>
                     </div>
                     <div className="justify-start items-center gap-4 inline-flex">
@@ -67,11 +82,9 @@ export default function Transactionmodal({isVisible, onClose}) {
                         </div>
                     </div>
                 </div>
-                <div className="justify-start items-start mt-6 inline-flex" >
-                    <button className="h-[40px] px-8 py-4 bg-stone-50 rounded-lg border-2 border-purple-400 justify-center items-center gap-2.5 flex text-purple-400 text-sm font-medium font-['Rubik'] uppercase " >
+                    <button className="justify-start items-start mt-4 inline-flex w-[110px] h-[40px] px-8 py-4 bg-stone-50 rounded-lg border-2 border-purple-400 justify-center items-center gap-2.5 flex text-purple-400 text-sm font-medium font-['Rubik'] uppercase " >
                         Close
                     </button>
-                </div>
             </div>
         </div>
     )
