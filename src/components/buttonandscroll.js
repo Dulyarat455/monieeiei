35a2d024097c0d230a3437a53e121fe8c54afd3f
Meuracha <uracharittikulsittichai@gmail.com>
2023-11-06 16:11:40 +0700

    solve issue

--- src/components/buttonandscroll.js
+++ src/components/buttonandscroll.js
@@ -22,10 +22,10 @@ export default function Buttonandscroll() {
                         <svg 
                         xmlns="http://www.w3.org/2000/svg" 
                         fill="none" viewBox="0 0 24 24" 
-                        stroke-width="2" 
+                        strokeWidth="2" 
                         stroke="currentColor" 
-                        class="w-6 h-6 m-auto">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75" />
+                        className="w-6 h-6 m-auto">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75" />
                         </svg>
                     </div>
                 </button>
--- src/components/searchandfilter.js
+++ src/components/searchandfilter.js
@@ -9,7 +9,7 @@ export default function Searchandfilter() {
         console.log('Check button clicked');
     }
     return(
-        <div style={{width:'screen', justifyContent:'space-between', padding:'0 30px'}} className="flex mt-4 gap-2">
+        <div style={{width:'screen', justifyContent:'space-between', padding:'0 30px'}} className="flex mt-4 gap-2 justify-center items-center">
         <div className="relative w-screen">
             <input style={{fontSize:'13px'}} className=" w-full h-[32px] p-2.5 bg-[#FFFEF9] rounded-lg border border-[#D9D9D9] focus:outline-none focus:border-purple-400 focus:ring-1 focus:ring-purple-400 placeholder-[#A6A6A6] placeholder:font-rubik pl-2 placeholder-text-xs placeholder-font-rubik placeholder-font-normal"
             placeholder="Search for"
@@ -19,10 +19,10 @@ export default function Searchandfilter() {
                 xmlns="http://www.w3.org/2000/svg" 
                 fill="none" 
                 viewBox="0 0 32 32" 
-                stroke-width="1.5" 
+                strokeWidth="1.5" 
                 stroke="#A7A7A7" 
                 className="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                 </svg>
             </button>
         </div>
