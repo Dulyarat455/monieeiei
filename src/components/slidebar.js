5ac107d34a95e95de6264e4a8744d5e77ff786cc
Dulyarat455 <Dulyarat.455@gmail.com>
2023-11-09 22:44:13 +0700

    edit

--- src/components/slidebar.js
+++ src/components/slidebar.js
@@ -15,38 +15,46 @@ export default function Slidebar() {
             setActiveDaily(true)
             setActiveBudget(false)
             setActiveSummary(false)
+            console.log("workspace_dailyexpense")
         }else if(currentPath === "/workspace_budget"){
             setActiveBudget(true)
             setActiveDaily(false)
             setActiveSummary(false)
+            console.log("workspace_budget")
         }else if(currentPath === "/workspace_summary"){
             setActiveSummary(true)
             setActiveBudget(false)
             setActiveDaily(false)
+            console.log("workspace_summary")
         }
     }, [currentPath]);
-    
+    //hover:bg-[#FAE392] hover:shadow-md
+    //hover:text-[#9B7C0D]
+    //{`${messagestatus ? "text-green-400": "text-red-500"} w-full text-left text-sm`}
     return(
         <nav style={{width:'100%', justifyContent:'space-between',padding:'0 30px'}} className="mt-3 h-[42px] justify-start items-center inline-flex">
                 <div className="flex-col justify-center items-center inline-flex">
                     <Link href="/workspace_dailyexpense" >
-                    <button className="p-1 ms-0 bg-[#FFFDF8] hover:bg-[#FAE392] hover:shadow-md border-2 border-[#FFDF6F] rounded-lg justify-center items-center inline-flex " >
-                        <div style={{fontSize:'11px'}} className="text-[#FFDF6F] hover:text-[#9B7C0D] text-xs font-medium font-rubik uppercase ">Daily Expense</div>
+                    <button className={`p-1 ms-0   ${activeDaily  ? "bg-[#FAE392] shadow-md": "bg-[#FFFDF8]"} border-2 border-[#FFDF6F] rounded-lg justify-center items-center inline-flex `}>
+                        <div style={{fontSize:'11px'}} className={` ${activeDaily  ? "text-[#9B7C0D]": "text-[#FFDF6F]"}      text-xs font-medium font-rubik uppercase `}>Daily Expense</div>
                     </button>
-                    <div style={{content:'" "', width: '87%', height:'4px', background:'#FAE392', bottom:'-2px'}} className="ms-1.5 mt-2 rounded-full"></div>
+                    { activeDaily === true && (
+                    <div style={{content:'" "', width: '87%', height:'4px', background:'#FAE392', bottom:'-2px'}} className="ms-1.5 mt-2 rounded-full"></div> )}
                     </Link>
                 </div>
                 <Link href="/workspace_budget">
-                    <button href="/workspace_budget" className="p-1 me-0 bg-[#FFFDF8] w-[140px] hover:bg-[#FAE392] hover:shadow-md rounded-lg border-2 border-[#FFDF6F] justify-center items-center gap-2 flex" >
-                        <div style={{fontSize:'11px'}} className="text-[#FFDF6F] hover:text-[#9B7C0D] text-xs font-medium font-rubik uppercase">Budget management</div>
+                    <button href="/workspace_budget" className={`p-1 me-0  w-[140px]    ${activeBudget  ? "bg-[#FAE392] shadow-md": "bg-[#FFFDF8]"}   rounded-lg border-2 border-[#FFDF6F] justify-center items-center gap-2 flex `}>
+                        <div style={{fontSize:'11px'}} className={`${activeBudget  ? "text-[#9B7C0D]": "text-[#FFDF6F]"} text-xs font-medium font-rubik uppercase`}>Budget management</div>
                     </button>
-                    <div style={{content:'" "', width: '85%', height:'4px', background:'#FAE392', bottom:'-2px'}} className="mt-2 ms-2.5 rounded-full"></div>
+                    { activeBudget === true &&
+                    (<div style={{content:'" "', width: '85%', height:'4px', background:'#FAE392', bottom:'-2px'}} className="mt-2 ms-2.5 rounded-full"></div>)}
                 </Link>
                 <Link href="/workspace_summary">
-                    <button href="/workspace_summary" className="p-1 bg-[#FFFDF8] hover:shadow-md hover:bg-[#FAE392] rounded-lg border-2 border-[#FFDF6F] justify-center items-center flex" >
-                        <div style={{fontSize:'11px'}} className="text-[#FFDF6F] hover:text-[#9B7C0D] text-xs font-medium font-rubik uppercase">Summary</div>
+                    <button href="/workspace_summary" className={`p-1  ${activeSummary  ? "bg-[#FAE392] shadow-md": "bg-[#FFFDF8]"} rounded-lg border-2 border-[#FFDF6F] justify-center items-center flex`}>
+                        <div style={{fontSize:'11px'}} className={`${activeSummary  ? "text-[#9B7C0D]": "text-[#FFDF6F]"} text-xs font-medium font-rubik uppercase`}>Summary</div>
                     </button>
-                    <div style={{content:'" "', width: '85%', height:'4px', background:'#FAE392', bottom:'-2px', left:'20px'}} className="mt-2 ms-1.5 rounded-full"></div>
+                    { activeSummary === true && (
+                    <div style={{content:'" "', width: '85%', height:'4px', background:'#FAE392', bottom:'-2px', left:'20px'}} className="mt-2 ms-1.5 rounded-full"></div> )}
                 </Link>
         </nav>
     )
