bcdf374bfd60d717b3a9a193964b2a2e20eae72f
Meuracha <uracharittikulsittichai@gmail.com>
2023-11-10 18:37:08 +0700

    edit slidebar make it full with any screen

--- src/components/slidebar.js
+++ src/components/slidebar.js
@@ -32,30 +32,34 @@ export default function Slidebar() {
     //hover:text-[#9B7C0D]
     //{`${messagestatus ? "text-green-400": "text-red-500"} w-full text-left text-sm`}
     return(
-        <nav style={{width:'100%', justifyContent:'space-between',padding:'0 30px'}} className="mt-3 h-[42px] justify-start items-center inline-flex">
-                <div className="flex-col justify-center items-center inline-flex">
-                    <Link href="/workspace_dailyexpense" >
-                    <button className={`p-1 ms-0   ${activeDaily  ? "bg-[#FAE392] shadow-md": "bg-[#FFFDF8]"} border-2 border-[#FFDF6F] rounded-lg justify-center items-center inline-flex `}>
+        <nav style={{width:'100%', justifyContent:'space-between',padding:'0 30px'}} className="mt-3 h-[42px] justify-start items-center inline-flex gap-1">
+                <div className="w-[100%] justify-start items-start inline-flex">
+                    <Link href="/workspace_dailyexpense" className="w-full">
+                    <button className={`p-1 w-[100px] sm:w-full ${activeDaily  ? "bg-[#FAE392] shadow-md": "bg-[#FFFDF8]"} border-2 border-[#FFDF6F] rounded-lg justify-center items-center inline-flex `}>
                         <div style={{fontSize:'11px'}} className={` ${activeDaily  ? "text-[#9B7C0D]": "text-[#FFDF6F]"}      text-xs font-medium font-rubik uppercase `}>Daily Expense</div>
                     </button>
                     { activeDaily === true && (
-                    <div style={{content:'" "', width: '87%', height:'4px', background:'#FAE392', bottom:'-2px'}} className="ms-1.5 mt-2 rounded-full"></div> )}
+                    <div style={{content:'" "', height:'4px', background:'#FAE392', bottom:'-2px'}} className="mt-2 ms-1 sm:ms-1.5 w-[90%] sm:w-[95%] rounded-full"></div> )}
                     </Link>
                 </div>
-                <Link href="/workspace_budget">
-                    <button href="/workspace_budget" className={`p-1 me-0  w-[140px]    ${activeBudget  ? "bg-[#FAE392] shadow-md": "bg-[#FFFDF8]"}   rounded-lg border-2 border-[#FFDF6F] justify-center items-center gap-2 flex `}>
+                <div className="w-full justify-start items-start inline-flex">
+                <Link href="/workspace_budget" className="w-full">
+                    <button href="/workspace_budget" className={`p-1 me-0 w-[140px] sm:w-full ${activeBudget  ? "bg-[#FAE392] shadow-md": "bg-[#FFFDF8]"}   rounded-lg border-2 border-[#FFDF6F] justify-center items-center gap-2 flex `}>
                         <div style={{fontSize:'11px'}} className={`${activeBudget  ? "text-[#9B7C0D]": "text-[#FFDF6F]"} text-xs font-medium font-rubik uppercase`}>Budget management</div>
                     </button>
                     { activeBudget === true &&
-                    (<div style={{content:'" "', width: '85%', height:'4px', background:'#FAE392', bottom:'-2px'}} className="mt-2 ms-2.5 rounded-full"></div>)}
+                    (<div style={{content:'" "',height:'4px', background:'#FAE392', bottom:'-2px'}} className="mt-2 ms-1.5 sm:ms-2 w-[90%] sm:w-[95%] rounded-full"></div>)}
                 </Link>
-                <Link href="/workspace_summary">
-                    <button href="/workspace_summary" className={`p-1  ${activeSummary  ? "bg-[#FAE392] shadow-md": "bg-[#FFFDF8]"} rounded-lg border-2 border-[#FFDF6F] justify-center items-center flex`}>
+                </div>
+                <div className="w-full justify-start items-start inline-flex">
+                <Link href="/workspace_summary" className="w-full">
+                    <button href="/workspace_summary" className={`p-1 w-full ${activeSummary  ? "bg-[#FAE392] shadow-md": "bg-[#FFFDF8]"} rounded-lg border-2 border-[#FFDF6F] justify-center items-center flex`}>
                         <div style={{fontSize:'11px'}} className={`${activeSummary  ? "text-[#9B7C0D]": "text-[#FFDF6F]"} text-xs font-medium font-rubik uppercase`}>Summary</div>
                     </button>
                     { activeSummary === true && (
-                    <div style={{content:'" "', width: '85%', height:'4px', background:'#FAE392', bottom:'-2px', left:'20px'}} className="mt-2 ms-1.5 rounded-full"></div> )}
+                    <div style={{content:'" "', height:'4px', background:'#FAE392', bottom:'-2px', left:'20px'}} className="mt-2 ms-1.5 sm:ms-2 w-[85%] sm:w-[95%] rounded-full"></div> )}
                 </Link>
+                </div>
         </nav>
     )
 }
