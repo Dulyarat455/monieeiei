ffb9e4e79d390d28391f68074ae56d776cac333e
Dulyarat455 <Dulyarat.455@gmail.com>
2023-11-14 22:42:57 +0700

    new kubb

---
+++ src/pages/api/budget/getbudgetmanagementfillter.js
@@ -0,0 +1,190 @@
+import { MongoClient, ServerApiVersion  } from 'mongodb';
+import jwt from 'jsonwebtoken';
+
+export default  async  function getbudgetmanagementfillter (req,res)  {
+
+    const uri = "mongodb+srv://Monie:1234@cluster0.jl8dvxy.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+    );
+
+
+    const token = req.headers["auth-token"];
+    const decoded =   jwt.decode(token)
+    const { user_id } = decoded || {};
+
+
+    const {
+
+        workspace_id,
+        dateIn,
+        dateOut
+        
+
+        } = req.body;
+
+    
+     if (req.method !== 'POST') {
+            return res.status(405).json({ message: 'Method not allowed', success: false });
+          }
+
+
+    try{
+       
+                await client.connect();
+                const budget  = client.db('monieeiei').collection('budget');
+                const transactions  = client.db('monieeiei').collection('Workspace transaction');
+                const proportion  = client.db('monieeiei').collection('Workspace proportion');
+         
+                let querydata = []
+                
+
+                // const areYearAndMonthEqual = (date1,date2) => {
+                //     const [year1, month1] = date1.split('-').map(Number);
+                //     const [year2, month2] = date2.split('-').map(Number);
+                //     console.log("year1 = ",year1)
+                //     console.log("year2 = ",year2)
+                //     console.log("month1 = ",month1)
+                //     console.log("month2 = ",month2)
+                //     return year1 === year2 && month1 === month2;
+                //   };
+
+
+
+                const getproportion  = await proportion.findOne({ workspace_id :  workspace_id },{})
+                const getbudget  = await budget.findOne({ workspace_id :  workspace_id },{})
+
+                if(getbudget.budget_value === 0){
+                    return ( res.status(400).json({message: 'No budget', success: false}))
+                }
+
+                querydata.push({
+                    $match:{
+                       'tran_type': 1
+                  }
+                   })
+
+                querydata.push(  {
+                    $match:{
+                       'workspace_id': workspace_id
+
+                  }
+                   })
+
+                   if(dateIn && dateOut){
+                    querydata.push( {
+                      '$match': {
+                        
+                          
+                            'bought_date': {
+                              $gte: dateIn,
+                              $lte: dateOut
+                            }
+                          
+                          
+                          }
+                    })
+                  }
+                
+                 
+                 
+
+                let gettransactions = await transactions.aggregate(querydata).toArray();
+
+
+                 if(!gettransactions){
+                    return ( res.status(400).json({message: 'gettransactions for budget failed', success: false}))
+                }
+
+                 let sumcategory = { food : 0 , utilities : 0 , entertainment : 0 , transport : 0, housing : 0,
+                    personal: 0 , medical : 0 , education : 0 , technlogy : 0 , saving : 0
+                }
+                 for (let i=0 ; i< gettransactions.length; i++){
+
+                        // sumcategory[gettransactions[i].category_name] = sumcategory[gettransactions[i].category_name] + gettransactions[i].amount
+                        if(gettransactions[i].category_name === "food"){
+                            sumcategory['food'] = sumcategory['food'] + gettransactions[i].amount
+                        }
+                        else if(gettransactions[i].category_name === "utilities"){
+                            sumcategory['utilities'] = sumcategory['utilities'] + gettransactions[i].amount
+                        }
+                        else if(gettransactions[i].category_name === "entertainment"){
+                            sumcategory['entertainment'] = sumcategory['entertainment'] + gettransactions[i].amount
+                        }
+                        else if(gettransactions[i].category_name === "transport"){
+                            sumcategory['transport'] = sumcategory['transport'] + gettransactions[i].amount
+                        }
+                        else if(gettransactions[i].category_name === "housing"){
+                            sumcategory['housing'] = sumcategory['housing'] + gettransactions[i].amount
+                        }
+                        else if(gettransactions[i].category_name === "personal"){
+                            sumcategory['personal'] = sumcategory['personal'] + gettransactions[i].amount
+                        }
+                        else if(gettransactions[i].category_name === "medical"){
+                            sumcategory['medical'] = sumcategory['medical'] + gettransactions[i].amount
+                        }
+                        else if(gettransactions[i].category_name === "education"){
+                            sumcategory['education'] = sumcategory['education'] + gettransactions[i].amount
+                        }
+                        else if(gettransactions[i].category_name === "technlogy"){
+                            sumcategory['technlogy'] = sumcategory['technlogy'] + gettransactions[i].amount
+                        }
+                        else if(gettransactions[i].category_name === "saving"){
+                            sumcategory['saving'] = sumcategory['saving'] + gettransactions[i].amount
+                        }
+
+                 }
+
+             let array = []   
+             let  arrayloop = ["food", "utilities", "entertainment", "transport", "housing",
+                "personal", "medical", "education" , "technlogy" , "saving"]
+             let arraypercent = [getproportion.percent0,getproportion.percent1,getproportion.percent2,getproportion.percent3
+             ,getproportion.percent4,getproportion.percent5,getproportion.percent6,getproportion.percent7,getproportion.percent8
+            ,getproportion.percent9] 
+              for (let i=0 ; i< arrayloop.length ; i++){    
+                    array.push({
+                    category_name : arrayloop[i] ,    
+                    budget: getbudget.budget_value * arraypercent[i]/100,
+                    actual: sumcategory[arrayloop[i]],
+                    remaining: (getbudget.budget_value * arraypercent[i]/100) - sumcategory[arrayloop[i]]
+
+                    })
+             }
+              
+               console.log(sumcategory)
+               console.log("array = ", array)
+
+                //  {
+                //     $match: {
+                //       $or: [
+                //         { 'user_id': user_id },
+                //         { 'tran_member': user_id}
+                //       ]
+                //     }
+                //   }
+                
+
+                if(array.length > 0 ){
+                    return( res.status(200).json({ message: 'Get budget management fillter success',array, success: true}))
+                    
+                }
+
+                else{
+                    return ( res.status(400).json({message: 'Get budget management fillter failed',gettransactions, success: false}))
+                }
+
+    }catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: error.message, success: false });
+      } finally {
+        await client.close();
+      }
+
+
+
+}
--- src/pages/workspace_budget.js
+++ src/pages/workspace_budget.js
@@ -13,16 +13,25 @@ import Workspacesetting from "@/components/workspacesetting";
 
 export default function Workspace_dailyexpense(){
     const [info, setInfo] = useState([]);
+    const [categorySearch, setCategorySearch] = useState("")
+    const [message, setMessage] = useState("");
+    const [messagestatus, setMessagestatus] = useState(false);
+    const [ownerStatus, setOwnerStatus] = useState(0);
+    const [fillter, setFillter] = useState({
+        workspace_id: "",
+        dateIn: "",
+        dateOut: ""
+      });
     
-    const handleIconClick = () => {
-        // Check is search icon ontop
-        console.log('Check button clicked');
-    }
+   
+    
 
     useEffect(() => {
         const token = localStorage.getItem("token");
         const workspaceId = localStorage.getItem("workspace_id");
-        
+        const getownerStatus = parseInt(localStorage.getItem("owner_status"));
+        setOwnerStatus(getownerStatus)
+        setFillter({ ...fillter, ["workspace_id"]: workspaceId });
         const res = fetch("/api/budget/getbudgetmanagement", {
             method: "POST",
             headers: {
@@ -48,7 +57,58 @@ export default function Workspace_dailyexpense(){
 
     }, []);    
 
+    const handleIconClick = () => {
+       
+        const token = localStorage.getItem("token");
+        const workspaceId = localStorage.getItem("workspace_id");
+        if((fillter["dateIn"] < fillter["dateOut"]) || (fillter["dateIn"] === fillter["dateOut"]) || (!fillter["dateIn"] && !fillter["dateOut"])){
+       
+            const res = fetch("/api/budget/getbudgetmanagementfillter", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "auth-token": token,
+            },
+                 body: JSON.stringify((fillter)),
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.success) {
+                 
+                 setInfo(data.array)
+                 setMessage("")
+
+
+                } else {
+                  console.log(data)
+                 
+                }
+            }
+            );
+
+        }
+        else{
+            setMessage("Wrong date selection format!")
+            setMessagestatus(false)
+      }  
+    }
+
+    const searchCategory = (e) => {
+        setCategorySearch(e.target.value)
+    }
+    const searchWord = (textToSearch, searchString) => {
+        // Case-insensitive search for the searchString in the textToSearch
+        const regex = new RegExp(searchString, 'i');
+        return regex.test(textToSearch);
+      };
+
+      const changeHandler = (e) => {
+        setFillter({ ...fillter, [e.target.name]: e.target.value });
+     }
+
     console.log("budget = ",info)
+    console.log("Categorysearch = ",categorySearch )
+    console.log("fillter = ",fillter)
 
     return(
         <div className={`min-h-screen bg-[#FFFEF9] ${inter.className}`}>
@@ -65,7 +125,9 @@ export default function Workspace_dailyexpense(){
                 <div className="relative w-screen inline-flex gap-1">
                     <input style={{fontSize:'13px'}} className=" w-full h-[32px] p-2.5 bg-[#FFFEF9] rounded-lg border border-[#D9D9D9] focus:outline-none focus:border-purple-400 focus:ring-1 focus:ring-purple-400 placeholder-[#A6A6A6] placeholder:font-rubik pl-2 placeholder-text-xs placeholder-font-rubik placeholder-font-normal"
                     placeholder="Search for"
-                    type="text"/>
+                    type="text"
+                    onChange={searchCategory}
+                    />
                     <button className="absolute right-10 translate-y-2 items-cente inline-flexr" onClick={handleIconClick}>
                         <svg 
                         xmlns="http://www.w3.org/2000/svg" 
@@ -79,11 +141,12 @@ export default function Workspace_dailyexpense(){
                     </button>
                     <Workspacesetting/>
                 </div>
-                <Link href="/setproportion">
+
+               {ownerStatus === 1 && <Link href="/setproportion">
                 <button className="px-3 py-2.5 h-[32px] bg-purple-300 hover:bg-purple-400 rounded-lg justify-center items-center gap-2.5 inline-flex text-white text-xs font-normal font-['Rubik']">
                 Proportion
                 </button>
-                </Link>
+                </Link>}
 
             </div>
             {/* Calendar */}
@@ -92,22 +155,32 @@ export default function Workspace_dailyexpense(){
                     <div style={{fontSize:'13px',whiteSpace: 'nowrap'}} className="text-neutral-400 text-xs font-normal font-['Rubik'] me-2 ml-2">Filter by:</div>
                     <input style={{fontSize:'13px'}} className="w-full sm:w-full md:w-full lg:w-full xl:w-full 2xl:w-full h-[32px] p-2.5 bg-[#FFFEF9] rounded-lg border border-[#D9D9D9] focus:outline-none focus:border-purple-400 focus:ring-1 focus:ring-purple-400 placeholder-[#A6A6A6] placeholder-text-xs text-neutral-400 placeholder-font-rubik placeholder-font-normal"
                     placeholder="MM/DD/YYYY" 
-                    type="date"/>
+                    type="date"
+                    name="dateIn"
+                    onChange={changeHandler}
+                    />
                     <div className="text-neutral-400 text-xs font-normal justify-center items-center font-['Rubik'] ml-2 me-2">-</div>
                     <input style={{fontSize:'13px',width:'100%'}} className="justify-end w-full sm:w-full md:w-full lg:w-full xl:w-full 2xl:w-full h-[32px] p-2.5 bg-[#FFFEF9] rounded-lg border border-[#D9D9D9] focus:outline-none focus:border-purple-400 focus:ring-1 focus:ring-purple-400 placeholder-[#A6A6A6] placeholder-text-xs text-neutral-400 placeholder-font-rubik placeholder-font-normal"
                     placeholder="MM/DD/YYYY"
-                    type="date"/>
+                    type="date"
+                    name="dateOut"
+                    onChange={changeHandler}
+                    />
                 </div>
             </div>
+            {message &&<div className="flex justify-center items-center ml-10 mt-1">
+                             <p className={`${messagestatus ? "text-green-400": "text-red-500"} w-full text-left text-sm`}>{message}</p>
+            </div>}
             <Navbarbottom />
             <Scroll />
             <div className="mb-24">
-            {info.map((budget,index) => (     
+            {categorySearch.length === 0  && info.map((budget,index) => (     
                 <Category key={index} category_name = {budget.category_name} budget = {budget.budget} actual = {budget.actual} remaining = {budget.remaining} />
+            ))} 
+            {categorySearch.length > 0  && info.map((budget,index) => (     
+                ( searchWord(budget.category_name,categorySearch) === true   && <Category key={index} category_name = {budget.category_name} budget = {budget.budget} actual = {budget.actual} remaining = {budget.remaining} />)
             ))}
-                {/* <Category />
-                <Category />
-                <Category /> */}
+               
             </div>
             
             {/*<div className="flex flex-col items-center justify-center h-full mt-2">
